ffd80ef6f38918e5ee0b85701169793240d09afd
binseent <ikingjabjab31@gmail.com>
2024-11-27 01:05:08 +0800

    modified:   frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-// import LoginPage from "./pages/Login/LoginPage";
-// import StudentDashboard from "./pages/Student/StudentDashboard";
- import AdminPage from "./pages/AdminPage/AdminPage.jsx";
+import LoginPage from "./pages/Login/LoginPage";
+import StudentDashboard from "./pages/Student/StudentDashboard";
+import AdminPage from "./pages/AdminPage/AdminPage.jsx";
 
 function App() {
   return (
-    // <> <StudentDashboard /> </>
-     <> <AdminPage /> </>
+    <>
+      {" "}
+      <StudentDashboard />{" "}
+    </>
+    //<> <AdminPage /> </>
     // <> <LoginPage /> </>
   );
 }
