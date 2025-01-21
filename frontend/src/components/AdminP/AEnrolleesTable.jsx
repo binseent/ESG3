dd04c28e3784f5159cb13d7d33c157111c6ccd62
Estremadura, Ric Michael <ricmichaele@gmail.com>
2025-01-21 22:19:50 +0800

    Update AEnrolleesTable.jsx

--- frontend/src/components/AdminP/AEnrolleesTable.jsx
+++ frontend/src/components/AdminP/AEnrolleesTable.jsx
@@ -177,7 +177,6 @@ const handleReject = (id) => {
               <th>Student ID</th>
               <th>Full Name</th>
               <th>Contact No.</th>
-              <th>Course</th>
               <th>Email</th>
               <th>Enrollment Status</th>
               <th>Actions</th>
